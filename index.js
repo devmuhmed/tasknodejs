7a00cedab4b3e7cf4a180336820cde056cf118fc
mohamed ibrahim <mohamedebrahim4221@gmail.com>
2021-06-17 08:19:29 +0200

    remove some comment

--- index.js
+++ index.js
@@ -20,20 +20,7 @@ mongoose.connect(`mongodb+srv://mohamed:${dbPass}@cluster0.n2rvd.mongodb.net/sho
             console.log(`listening on port ${PORT}`)
         })
     })
-
-    // .catch((e)=>{
-    //     console.error(e)
-    // })
-    //enhanced to be that meaning first parameter in console will pass to first paramter to error 
     .catch(console.error) //it knows theat it will take first parameter and put in error message  
 
 
 
-    //for testing module
-    //const User =require("./models/users") 
-    //for testin add data in module records
-    // const newUser = new User ({name:"Mohamed",email:"mohamedebrahim4221@gmail.com",password:"12345"})
-    // return newUser.save()
-    // middleware prevent any enter another req
-    // if(req.url !== "/users") return res.status(400).send("only send request to /users")
-    // console.log("Hello from the middleware")
--- middleware/authnticate.js
+++ middleware/authnticate.js
@@ -1,13 +1,6 @@
 const jwt =require("jsonwebtoken")
 const secretKey=process.env.secretKey
 const aut = async (req, res, next) => {
-    // const { email , password} = req.body;
-    // const user = await User.findOne({ email })
-    // if (!user) return res.status(401).send("Invalid email or password");
-    // const vPass = await bcrypt.compare(password , user.password)
-    // if (!vPass) return res.status(401).send("Invalid password");
-    // next();
-    //Enhance it by using token 
     const token = req.headers.token
     try{
         jwt.verify(token,secretKey)
